eca8da15cb75b801bee2eebe31f097d80d80dfc9
aashis-kishore <aashiskishore@gmail.com>
2021-08-18 02:31:38 +0530

    Test request body title

--- jest.config.ts
+++ jest.config.ts
@@ -32,7 +32,7 @@ export default {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -94,7 +94,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -138,7 +138,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: 'jest-environment-node'
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
--- test/sample.spec.ts
+++ test/sample.spec.ts
@@ -1,5 +1,18 @@
+const superagent = require('superagent')
 
-test('should succeed', () => {
-  const x = true
-  expect(x).toBeTruthy()
+interface Post {
+  id: number
+  title: string
+  author: string
+}
+
+test('should have expected title', async () => {
+  try {
+    const res = await superagent.get('http://localhost:3000/posts/1')
+    expect(res.status).toBe(200)
+    const { title } = <Post> res.body
+    expect(title).toBe('json-server')
+  } catch (err) {
+    console.error(err)
+  }
 })
